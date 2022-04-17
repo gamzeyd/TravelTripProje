898de895ba822f552c29af7ff75085b740dff829
gamzeyd <77302387+gamzeyd@users.noreply.github.com>
2022-04-17 16:09:28 +0300

    Add project files.

---
+++ TravelTripProje/Models/Sınıflar/Yorumlar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Sınıflar
+{
+    public class Yorumlar
+    {
+        public int ID { get; set; }
+        public String KullaniciAdi { get; set; }
+        public string Mail  { get; set; }
+        public string Yorum  { get; set; }
+        public Blog Blog { get; set; }
+       
+        
+
+    }
+}
